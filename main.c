5f18f984567b4a6da7114830cf5837ab7c33e6a6
Mykola Karpets <kol9karpec@gmail.com>
2017-06-17 15:35:40 +0300

    Implemented task #3
    Signed-off-by: Mykola Karpets <kol9karpec@gmail.com>

--- main.c
+++ main.c
@@ -3,8 +3,14 @@
 #include <string.h>
 
 typedef void* (*memset_ptr)(void * str, int c, size_t n);
-
 volatile memset_ptr memset_s = &memset;
+
+size_t _strlen(const char * s);
+size_t _strnlen(const char * s, size_t maxlen);
+
+char* _strchr(const char * s, int c);
+char* _strrchr(const char * s, int c);
+
 
 int main() {
 
@@ -17,5 +23,68 @@ int main() {
 	/*memset_s((void*)memset_s,0,sizeof(memset_s));
 	printf("%p\n",memset_s);*/ //Seg fault ?? don't know why
 
+	char str[] = "qwertyuiopasdfghjklzxcvbnm\0";
+
+	printf("Length of str: %d\n",(int)_strlen(str));
+	printf("Length of str (_strnlen(str,24)): %d\n",(int)_strnlen(str,24));
+
+	if( _strchr(str,'a') )
+		printf("'a' found!\n");
+	if( _strchr(str,'6'))
+		printf("'6' found!\n");
+
 	return 0;
 }
+
+size_t _strlen(const char * s) {
+	size_t 	result = 0,
+		i = 0;
+
+	while(s[i++]) {
+		result++;
+	}
+
+
+	return result;
+}
+
+size_t _strnlen(const char * s, size_t maxlen) {
+	size_t	result = 0,
+		i = 0;
+
+	while(s[i] && i < maxlen) {
+		result++;
+		i++;
+	}
+
+
+	return result;
+}
+
+char* _strchr(const char * s, int c) {
+	char	*result = NULL;
+	int 	i = 0;
+
+	while(s[i]) {
+		if((int)s[i] == c) {
+			result = (char*)(&s[i]);
+		}
+		i++;
+	}
+	return result;
+}
+
+char* _strrchr(const char * s, int c){
+	char	*result = NULL;
+	int 	i = _strlen(s)-1;
+
+	while(s[i]) {
+		if((int)s[i] == c) {
+			result = (char*)(&s[i]);
+		}
+		i--;
+	}
+	return result;
+}
+
+
